8ec14fa4d730537306ce83bbfe17129cdf0b1250
xiangshouding <fansekey@gmail.com>
2014-06-06 07:57:44 +0800

    0.1.0

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * fis
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+
+var CleanCss = require('clean-css');
+
+module.exports = function(content, file, conf){
+    conf.processImport = false;
+    return new CleanCss(conf).minify(content);
+};
