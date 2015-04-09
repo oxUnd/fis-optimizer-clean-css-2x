0f74e15ee8fb86bde0982824193995a1af1c7641
xiangshouding <fansekey@gmail.com>
2015-04-09 14:43:17 +0800

    update

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var CleanCss = require('clean-css');
 
 module.exports = function(content, file, conf){
     conf.processImport = false;
-    return new CleanCss(conf).minify(content);
+    return new CleanCss(conf).minify(content).styles;
 };
